3e266a6c3230d59c5d61f613f05a01af26970607
Khalid Ajlan <khalidhh85@gmail.com>
2019-10-06 03:07:46 +0300

    added IBAction for the Pick button

--- MemeMe V1.0/ViewController.swift
+++ MemeMe V1.0/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func pickAnImage(_ sender:Any) {
+
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+
     }
 
 
