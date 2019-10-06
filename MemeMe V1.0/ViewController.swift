918c273f9dab4b5c50838c781a9890966c584c86
Khalid Ajlan <khalidhh85@gmail.com>
2019-10-06 03:44:26 +0300

    added the method for picking from camera and a camera button

--- MemeMe V1.0/ViewController.swift
+++ MemeMe V1.0/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func pickAnImage(_ sender:Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    @IBAction func pickAnImageFromAlbum(_ sender:Any) {
 
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+         pickerController.sourceType = .photoLibrary
         present(pickerController, animated: true, completion: nil)
 
+    }
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
      func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
